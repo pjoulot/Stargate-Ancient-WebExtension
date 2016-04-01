e8ff7de8ae6abbb094830671d39aa795b0ee3496
Philippe Joulot <phjou@smile.fr>
2016-04-01 11:59:56 +0200

    Enable/Disable click works, and save the current state to restore it when the browser is launched

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 ## Description ##
 
 The extension changes the font of the page by the ancient one from Stargate TV Show.
+
+## Compatibility ##
+
+- Mozilla Firefox 45 and more
+- Google Chrome 41 and more
+
+## Versions ##
+0.7
+- Change the font by the ancient one when clicking the extension button
+0.8 
+- Use the ancient font by default if the extension is installed and enabled
+- Enable / Disable the font change clicking the extension button
+- Save the state "disabled" or "enabled" and restore it when the browser is launched
+
+## Need to be developped ##
+
+- Change logos, use stargate ones [Needed for v1]
+- Create an helper popup with the matching between ancient letters and the latin ones.
--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,12 +1,32 @@
+var enable = 1;
+
+//local could be change by sync when Firefox will have implemented it
+chrome.storage.local.get('stargate-ancient-font', function (obj) {
+  if(typeof obj['stargate-ancient-font'] != 'undefined') {
+    enable = obj['stargate-ancient-font'];
+  }
+});
+
+//Apply the display changes each time the tab is updated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if(enable) {
+    var chosenBeastURL = chrome.extension.getURL("fonts/anquietas.ttf");
+
+    chrome.tabs.executeScript(null, {
+      file: "/content_scripts/font_changes.js"
+    });
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {beastURL: chosenBeastURL});
+    });
+  }
+});
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var chosenBeastURL = chrome.extension.getURL("fonts/anquietas.ttf");
-
-  chrome.tabs.executeScript(null, {
-    file: "/content_scripts/font_changes.js"
-  });
-
+  enable = (enable + 1) % 2;
+  chrome.storage.local.set({'stargate-ancient-font': enable}, function() {});
+  //Reload the current tab to apply changes
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {beastURL: chosenBeastURL});
+    chrome.tabs.reload(tabs[0].id);
   });
 });
